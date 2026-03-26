6a53715c5def03d0940ab034d6164dddd3ff375e
Prithviraj Patil <prithvirajpatil242@gmail.com>
2026-03-26 17:20:55 +0530

    Added prefix sum approach for max subarray

--- arrays/maxSumOfSubarray2.java
+++ arrays/maxSumOfSubarray2.java
@@ -1,25 +1,31 @@
 import java.util.*;
 
-public class maxSumOfSubarray1 {
+public class maxSumOfSubarray2 {
 
     public static void maxSumSubarray(int numbers []){
         int currSum = 0;
         int maxSum  = Integer.MIN_VALUE;
+        int prefix[] = new int[numbers.length];
+
+        prefix[0] = numbers[0];
+
+        for(int i = 1 ; i < numbers.length;i++){
+            prefix[i]=prefix[i-1]+numbers[i];
+
+        }    
+
+
 
         for(int i = 0 ; i < numbers.length;i++){
             
             for(int j = i ; j < numbers.length;j++){
-                currSum = 0;
-                for(int k = i ; k <= j;k++){
-                    //subarray sum
-                    currSum += numbers[k];
-                }
+                
+                currSum = i == 0? prefix[j] : prefix[j] - prefix[i-1];
+                
+
                 System.out.println(currSum);
                 if(maxSum < currSum){
-                      maxSum = currSum ;
-                     
-                      
-                      
+                     maxSum = currSum ;
                 }
             }
             
@@ -37,4 +43,3 @@ public class maxSumOfSubarray1 {
 }
 
     
-
