607e2ab879e09910be05698493cf48e1a532987a
Prithviraj Patil <prithvirajpatil242@gmail.com>
2026-04-01 19:02:10 +0530

    learned all type of sorting from basics

--- sorting/sorting.java
+++ sorting/sorting.java
@@ -45,7 +45,28 @@ public class sorting {
         }
     }
 
-    public static void printArr(Integer arr[]){
+    public static void countingSort(int arr[]){
+        int largest=Integer.MIN_VALUE;
+        for(int i=0; i<arr.length;i++){
+            largest = Math.max(largest, arr[i]);
+        }
+        
+        int count[]=new int[largest+1];
+        for(int i =0; i < arr.length;i++){
+            count[arr[i]]++;
+        }
+
+        int j = 0;
+        for(int i=0;i<count.length;i++){
+            while(count[i] > 0){
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
+
+    public static void printArr(int arr[]){
         for(int i=0; i<arr.length; i++){
             System.out.print(arr[i] + " ");
         }
@@ -54,8 +75,8 @@ public class sorting {
 
     public static void main (String args[]){
       
-        Integer arr[] ={5,2,3,4,1};
-        Arrays.sort(arr,Collections.reverseOrder());
+        int arr[] ={5,2,3,4,1};
+        countingSort(arr);
         printArr(arr);
          
 
