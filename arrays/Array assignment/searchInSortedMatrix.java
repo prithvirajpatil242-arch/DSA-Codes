114b56eedc9c68a011b2a0de239a026dbf65666d
Prithviraj Patil <prithvirajpatil242@gmail.com>
2026-04-07 18:41:47 +0530

    searching using staircase method in sorted matrix

---
+++ arrays/Array assignment/searchInSortedMatrix.java
@@ -0,0 +1,34 @@
+public class searchInSortedMatrix {
+
+    public static boolean staircaseSearch(int matrix[][], int key){
+        int row = 0 ; int col = matrix.length-1;
+
+        while(row<matrix.length && col >= 0){
+            if(matrix[row][col] == key){
+                System.out.println("Key found at ("+ row +","+col+")");
+                return true;
+            }
+            else if(key <matrix[row][col]){
+                col--;
+            }
+            else {
+                row++;
+            }
+        }
+        System.out.println("key not found");
+        return false;
+    }
+
+    public static void main (String args[]){
+        int matrix[][] = {{10,20,30,40},
+                          {15,25,35,45},
+                          {27,29,37,48},
+                          {32,33,49,50}};
+        
+        int key = 33; 
+        
+        staircaseSearch(matrix, key);
+        
+    }
+    
+}
