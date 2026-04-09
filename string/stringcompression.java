0981c848d750f0086b037236390541e2bbd1c446
Prithviraj Patil <prithvirajpatil242@gmail.com>
2026-04-09 07:26:53 +0530

    string compresion using stringbuilder data typr

---
+++ string/stringcompression.java
@@ -0,0 +1,31 @@
+public class stringcompression {
+    public static String compress(String str){
+        StringBuilder newStr = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            int count = 1;
+
+            while (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
+                count++;
+                i++;
+            }
+
+            newStr.append(str.charAt(i));
+
+            if (count > 1) {
+                newStr.append(count);
+            }
+        }
+
+        return newStr.toString();
+ 
+    }
+    public static void main (String args[]){
+        String str = "aaaabbbbcccdd";
+
+        System.out.println(compress( str));
+
+    }
+
+    
+}
