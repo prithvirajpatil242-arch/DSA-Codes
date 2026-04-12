497933bca289aab8a221714a954d127841ab23c4
Prithviraj Patil <prithvirajpatil242@gmail.com>
2026-04-12 23:16:01 +0530

    implrmrnted c ode to check wheather two strings are  anagram or not

---
+++ string/AnagramCheck.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+ import java.util.Scanner;
+
+
+public class AnagramCheck { 
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first string: ");
+        String str1 = sc.nextLine();
+
+        System.out.print("Enter second string: ");
+        String str2 = sc.nextLine();
+
+        // Convert to lowercase (to ignore case)
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+
+        // Convert strings to char arrays
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+
+        // Sort both arrays
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        // Compare arrays
+        if (Arrays.equals(arr1, arr2)) {
+            System.out.println("The strings are anagrams.");
+        } else {
+            System.out.println("The strings are NOT anagrams.");
+        }
+    }
+}
+    
+
+
+    
+
